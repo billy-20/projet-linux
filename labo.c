bf87ead3fbe80d7061e51ee9feed145ca74c8ffc
bilal el haddadi <bilal.el.haddadi77@gmail.com>
2023-05-13 14:58:34 +0200

    update all

--- labo.c
+++ labo.c
@@ -5,52 +5,75 @@
 #include <signal.h>
 #include <sys/types.h>
 #include <sys/wait.h>
-
 #define NUM_ZOMBIES 2
-
 #include "messages.h"
 #include "utils_v2.h"
 
-void arreter_zombie(pid_t* zombies, int numZombies) {
+int client_sock;
+
+void handle_sigquit(int sig)
+{
+    char *shutdown_message = "LABO_SHUTDOWN\n";
+    write(client_sock, shutdown_message, strlen(shutdown_message));
+    close(client_sock);
+    exit(0);
+}
+
+void arreter_zombie(pid_t *zombies, int numZombies)
+{
     int i;
-    for (i = 0; i < numZombies; i++) {
+    for (i = 0; i < numZombies; i++)
+    {
         kill(zombies[i], SIGTERM);
     }
 
-    for (i = 0; i < numZombies; i++) {
+    for (i = 0; i < numZombies; i++)
+    {
         waitpid(zombies[i], NULL, 0);
     }
 }
 
-int main() {
+int main()
+{
+    struct sigaction sa_quit;
+
+    sa_quit.sa_handler = handle_sigquit;
+    sigemptyset(&sa_quit.sa_mask);
+    sigaction(SIGQUIT, &sa_quit, NULL);
+
     pid_t zombies[NUM_ZOMBIES];
 
-    int ports_dispo[] = {1024, 1025, 1026, 1027, 1028, 1029, 1030, 1031, 1032, 1033, 1034};
+    int ports_dispo[] = { 1024, 1025, 1026, 1027, 1028, 1029, 1030, 1031, 1032, 1033, 1034 };
 
-    int i;
-    for (i = 0; i < NUM_ZOMBIES; i++) {
+    for (int i = 0; i < NUM_ZOMBIES; i++)
+    {
         pid_t pid = fork();
-        if (pid == -1) {
+        if (pid == -1)
+        {
             printf("error fils");
             arreter_zombie(zombies, i);
             return 1;
-        } else if (pid == 0) {
+        }
+        else if (pid == 0)
+        {
             char portArg[10];
-            sprintf(portArg,"%d", ports_dispo[i]);
+            sprintf(portArg, "%d", ports_dispo[i]);
             execl("./zombie", "zombie", NULL);
 
-            printf("Erreur lors de l'exécution du zombie");
+            printf("Erreur dans le zombie");
             exit(1);
-        } else {
+        }
+        else
+        {
             zombies[i] = pid;
         }
     }
 
-    printf("Ctrl+D pour arrêter le laboratoire...\n");
-    while (getc(stdin) != EOF) {
+    for (int i = 0; i < NUM_ZOMBIES; i++)
+    {
+        int status;
+        waitpid(zombies[i], &status, 0);
     }
-
-    arreter_zombie(zombies, NUM_ZOMBIES);
 
     return 0;
 }
